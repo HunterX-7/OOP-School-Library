4cbc2325ada9a70d252a2fd86ced63ff051a03dc
HunterX-7 <fraquin7@gmail.com>
2023-03-15 18:31:22 -0500

    Test changes

--- classes/test.rb
+++ classes/test.rb
@@ -1,10 +1,29 @@
-require_relative 'person'
-require_relative 'capitalize'
-require_relative 'trimmer'
+require_relative 'classroom'
+require_relative 'student'
+require_relative 'book'
+require_relative 'rental'
 
-puts person = Person.new(22, 'maximilianus')
-puts person.correct_name
-puts capitalized_person = CapitalizeDecorator.new(person)
-puts capitalized_person.correct_name
-puts capitalize_trimmed_person = TrimmerDecorator.new(capitalized_person)
-puts capitalize_trimmed_person.correct_name
+
+max = Student.new(22, 'maximilianus')
+ericka = Student.new(20, 'ericka')
+
+fisica = Classroom.new('Physics')
+
+iliada = Book.new('iliada', 'Homero')
+biblia = Book.new('bible', 'Jesus')
+
+book_rent_one = Rental.new(max, biblia, '2020-12-02')
+book_rent_two = Rental.new(ericka, iliada, '2022-10-07')
+
+book_rent_one.date
+book_rent_two.date
+
+fisica.add_student(max)
+ericka.classroom = fisica
+
+puts(fisica.students.each_with_index.map { |student, index| "#{index + 1}. #{student.name}" })
+
+puts(biblia.rentals.map { |rental| rental.person.name })
+puts(ericka.rentals.map { |rental| rental.person.name })
+
+puts(max.rentals.map { |rental| rental.date })
