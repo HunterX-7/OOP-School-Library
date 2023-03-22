68b8ac03e9e73d1134c2264cbae005a63deb0881
HunterX-7 <fraquin7@gmail.com>
2023-03-22 11:23:39 -0500

    Add the to_json method to store the data in json

--- classes/book.rb
+++ classes/book.rb
@@ -1,4 +1,5 @@
 require_relative 'rental'
+require 'json'
 
 class Book
   attr_accessor :title, :author
@@ -18,5 +19,10 @@ class Book
     list = ''
     @rentals.each { |rental| list << "\n|#{rental.person.name} | #{rental.book.title} | #{rental.date}|" }
     list << "\n\n"
+  end
+
+  def to_json(*_args)
+    hash = { title: @title, author: @author }
+    hash.to_json
   end
 end
